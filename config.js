846ef1e8b1aa63128531d02c1b5d8b81f8609a27
khuyenrua1 <115389075+khuyenrua1@users.noreply.github.com>
2022-10-09 16:08:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'cậuu à!',
+    introTitle: 'cậu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tuii lẻ loi
+    Lạc vào tim tui lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích tuii <33',
-    btnNo: 'Không, bạnn à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tuii vậy <3',
-    btnReply: 'Gửi cho tuii <3',
+    btnYes: 'Vẫn cứ là thích tui <33',
+    btnNo: 'Không, cậu à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tui vậy <3',
+    btnReply: 'Gửi cho tui <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'tuii biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Tối nay 8h tuii qua đón nhé cậuuu.',
